187e37838a6ec567b1f4608300a7a08f68774072
max <maximilianwu0828@gmail.com>
2024-07-02 20:56:04 -0700

    assets for navbar and navbar styling

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -1,12 +1,30 @@
 import { Link } from 'react-router-dom';
 import './index.css';
 
+// assets
+import logo from '../../assets/pintstagram-logo.png';
+import house from '../../assets/ig-house-logo.png'
+
 const Navbar = () => {
   return (
     <div className="container">
-      <Link to="/">
-        <h1>Pintstagram</h1>
-      </Link>
+      <div className="logo-container">
+        <Link to="/">
+          <div className="logo-wrapper">
+              <img src={ logo } alt="Logo" className="logo" />
+          </div>            
+        </Link>
+      </div>
+
+      <div className="nav-container">
+        <Link to="/">
+          <div className="nav-options">
+            <img src={ house } alt="House" />
+            <h2>Home</h2>
+          </div>
+        </Link>        
+      </div>
+
     </div>
   );
 }
