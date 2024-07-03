9e574d6e99cc15c18c95bafe86d8968fd4463d07
max <maximilianwu0828@gmail.com>
2024-07-02 18:07:25 -0700

    added semicolons

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,16 +1,16 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
 
 // controller functions
-const { loginUser, signupUser } = require('../controllers/userController')
+const { loginUser, signupUser } = require('../controllers/userController');
 
 
 // login route
-router.post('/login', loginUser)
+router.post('/login', loginUser);
 
 // signup route
-router.post('/signup', signupUser)
+router.post('/signup', signupUser);
 
 
-module.exports = router
+module.exports = router;
