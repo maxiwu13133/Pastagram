49f37f55aeb9bf1607d166a1153799553c167b45
max <maximilianwu0828@gmail.com>
2024-07-28 17:34:04 -0700

    require auth middleware

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const requireAuth = require('../middleware/requireAuth');
 
 
 // controller functions
 const { loginUser, signupUser, getCommunity, followUser, unfollowUser } = require('../controllers/userController');
-
 
 // login route
 router.post('/login', loginUser);
