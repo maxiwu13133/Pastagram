fb5773bb579d055bb28331b866e509fd32762b94
max <maximilianwu0828@gmail.com>
2024-08-17 15:05:25 -0700

    database management and post creation

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -3,10 +3,12 @@ const Post = require('../models/postModel');
 // create post
 const createPost = async (req, res) => {
   const user = req.user;
+  const { photos, caption } = req.body;
 
   try {
+    const post = await Post.create({ user_id: user._id, photos, caption });
 
-    res.status(200).json({ data: req.body })
+    res.status(200).json({ post });
   } catch (error) {
     res.status(400).json({ error: error.message });
   };
