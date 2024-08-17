2fb2f3dcaf8b3e524bfd0bdc181106e7c45ca17d
max <maximilianwu0828@gmail.com>
2024-08-17 13:55:20 -0700

    upload to cloudinary and post to database with res link

--- frontend/src/hooks/useUpload.js
+++ frontend/src/hooks/useUpload.js
@@ -1,16 +1,23 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 
 export const useUpload = () => {
+  const { user } = useAuthContext();
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(null);
   const [postedImages, setPostedImages] = useState([]);
-  
+  const [caption, setCaption] = useState('');
+  const [fileLength, setFileLength] = useState(null);
+
 
   const upload = async (files, caption) => {
     setIsLoading(true);
     setError(null);
+    setCaption(caption);
+    setFileLength(files.length);
 
+    // upload to cloudinary
     for (const file of files) {
       const data = new FormData();
       data.append('file', file);
@@ -22,14 +29,48 @@ export const useUpload = () => {
       }).then(response => {
         return response.json();
       }).then(data => {
-        setPostedImages(postedImages => [...postedImages, data.url]);
+        setPostedImages(currentImages => [...currentImages, data.url]);
       }).catch(error => {
-        console.log('Error:', error);
+        setError(error);
       })
     };
-
-    console.log(postedImages);
   }
 
-  return { upload, error, isLoading};
+  useEffect(() => {
+    const createPost = async () => {
+      if (!error) {
+        const data = {
+          photos: postedImages,
+          caption
+        }
+  
+        const response = await fetch('http://localhost:4000/api/post/', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${ user.token }`
+          },
+          body: JSON.stringify(data)
+        })
+        const json = await response.json();
+    
+        if (!response.ok) {
+          setError(json.error);
+        }
+        if (response.ok) {
+          console.log(json);
+          setIsLoading(false);
+          setPostedImages([])
+        }
+      }
+    }
+
+    if (postedImages.length === fileLength) {
+      createPost();
+    }
+
+  }, [postedImages])
+  
+
+  return { upload, error, isLoading };
 };
