690294f39a33174864ca4fffa01b6ac32d372e81
max <maximilianwu0828@gmail.com>
2024-08-19 14:36:20 -0700

    posts displayed on profile page of user

--- frontend/src/components/Posts/PostsRow/index.js
+++ frontend/src/components/Posts/PostsRow/index.js
@@ -3,12 +3,11 @@ import './index.css';
 // components
 import Post from '../Post';
 
-const PostsRow = ({ posts }) => {
-  console.log(posts);
+const PostsRow = ({ chunk }) => {
 
   return (
-    <div className="profilerow-container">
-
+    <div className="postrow-container">
+      { chunk.map((post, index) => <Post key={ index } post={ post } />)}
     </div>
   )
 }
--- frontend/src/components/Posts/index.js
+++ frontend/src/components/Posts/index.js
@@ -1,9 +1,27 @@
 import './index.css';
 
-const Posts = ({ posts}) => {
+// components
+import PostsRow from './PostsRow';
+
+const Posts = ({ posts }) => {
+
+  const reversedPosts = [...posts].reverse();
+  
+  // segment posts into rows of 3 posts
+  const getPostChunks = (posts) => {
+    const chunks = []
+    for (let i = 0; i < posts.length; i += 3) {
+      const chunk = posts.slice(i, i + 3);
+      chunks.push(chunk);
+    }
+    return chunks;
+  }
+  const postChunks = getPostChunks(reversedPosts);
+
+  
   return (
     <div className="posts-container">
-      
+      { postChunks.map((chunk, index) => <PostsRow key={ index } chunk={ chunk } />)}
     </div>
   )
 
